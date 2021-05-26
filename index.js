54d951e9665e25e56d6e811f2c3374efc2ad8765
unknown <alexcerlinii@gmail.com>
2021-05-26 17:45:24 -0300

    Query params

--- index.js
+++ index.js
@@ -2,10 +2,17 @@ const express = require('express')
 
 const server = express()
 
+// Query params = ?teste=1
+// Route params = /users/1
+// Request body = { "name": "alexander", "email": "alexcerlinii@gmail.com"}
 
-//localhost:3000/teste:
+
+//localhost:3000
 server.get('/teste', (req, res) => {
-return res.json({message:'Hello'})
+const name = req.query.name
+
+return res.json({message: `Hello ${name}`})
 })
 
+//localhost:3000
 server.listen(3000)
