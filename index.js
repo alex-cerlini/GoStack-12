8b2b716fa911c1058094a71fee4be75f4a7a9c79
unknown <alexcerlinii@gmail.com>
2021-05-26 17:39:31 -0300

    Create the first app

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 const express = require('express')
+
+const server = express()
+
+
+//localhost:3000/teste:
+server.get('/teste', (req, res) => {
+return res.json({message:'Hello'})
+})
+
+server.listen(3000)
