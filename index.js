8ad56a9740ff6ca7f11e3e80dc475b8c5a637d05
unknown <alexcerlinii@gmail.com>
2021-05-27 12:18:49 -0300

    using insomnia so add array and index to test

--- index.js
+++ index.js
@@ -6,12 +6,14 @@ const server = express()
 // Route params = /users/1
 // Request body = { "name": "alexander", "email": "alexcerlinii@gmail.com"}
 
+const users = ['Alexander', 'Beatriz', 'Carla']
+
 
 //localhost:3000
-server.get('/users/:id', (req, res) => {
-const { id } = req.params
+server.get('/users/:index', (req, res) => {
+const { index } = req.params
 
-return res.json({message: `Buscando o usuário de id ${id}`})
+return res.json(users[index])
 })
 
 //localhost:3000
